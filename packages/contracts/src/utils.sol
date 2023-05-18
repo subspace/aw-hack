bc84cb5c10947c88b6c559307a4fdffbba0f9641
i1i1 <vanyarybin1@live.ru>
2023-05-18 21:58:58 +0300

    `forge fmt`

--- packages/contracts/src/utils.sol
+++ packages/contracts/src/utils.sol
@@ -2,9 +2,9 @@
 pragma solidity >=0.8.0;
 
 function addressToEntityKey(address addr) pure returns (bytes32) {
-  return bytes32(uint256(uint160(addr)));
+    return bytes32(uint256(uint160(addr)));
 }
 
 function positionToEntityKey(uint32 x, uint32 y) pure returns (bytes32) {
-  return keccak256(abi.encode(x, y));
+    return keccak256(abi.encode(x, y));
 }
