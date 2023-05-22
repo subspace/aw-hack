0f59f456ee1d9c33d1cc6295c6110ee8d9ff8a24
Serge Kovbasiuk <isserge@protonmail.com>
2023-05-22 15:47:06 +0400

    Move music into separate component, persist play music choice

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,9 @@
-import { SyncState } from "@latticexyz/network";
-import { useComponentValue } from "@latticexyz/react";
-import useSound from "use-sound";
+import { SyncState } from '@latticexyz/network';
+import { useComponentValue } from '@latticexyz/react';
 
-import { useMUD } from "./MUDContext";
-import { Grid } from "./Grid";
-
-// music
-import adventureStartSong from "./assets/music/adventureStarts.ogg";
-import { useState } from "react";
-import StopIcon from "./icons/StopIcon";
-import PlayIcon from "./icons/PlayIcon";
+import { useMUD } from './MUDContext';
+import { Grid } from './Grid';
+import { MusicButton } from './MusicButton';
 
 export const App = () => {
   const {
@@ -17,62 +11,30 @@ export const App = () => {
     network: { singletonEntity },
   } = useMUD();
 
-  const [playMusic, setPlayMusic] = useState(false);
-  const [play, { stop }] = useSound(adventureStartSong, {
-    interrupt: true,
-    volume: 0.5,
-    loop: true,
-    autoplay: true,
-    onplay: () => setPlayMusic(true),
-    onstop: () => setPlayMusic(false),
-    onpause: () => setPlayMusic(false),
-  });
-
   const loadingState = useComponentValue(
     components.LoadingState,
     singletonEntity,
     {
       state: SyncState.CONNECTING,
-      msg: "Connecting",
+      msg: 'Connecting',
       percentage: 0,
     }
   );
 
-  const handleMusic = () => {
-    if (playMusic) {
-      stop();
-    } else {
-      play();
-    }
-  };
-
   return (
-    <div className=" bg-black w-screen h-screen flex items-center justify-center ">
-      <div className="flex flex-col gap-10">
+    <div className="bg-black w-screen h-screen flex items-center justify-center relative overflow-hidden">
+      <div className="flex flex-col gap-10 h-full overflow-auto">
         {loadingState.state !== SyncState.LIVE ? (
           <div>
             {loadingState.msg} ({Math.floor(loadingState.percentage)}%)
           </div>
         ) : (
-          <div>
+          <div className="overflow-auto max-h-full">
             <Grid />
           </div>
         )}
-        <div>
-          <button
-            className="bg-white color-black px-2 py-2 rounded-full"
-            onClick={handleMusic}
-          >
-            {playMusic ? (
-              <span className="w-4 h-4">
-                <StopIcon />
-              </span>
-            ) : (
-              <span className="w-4 h-4">
-                <PlayIcon />
-              </span>
-            )}
-          </button>
+        <div className="absolute top-0 right-0 m-4">
+          <MusicButton />
         </div>
       </div>
     </div>
---
+++ packages/client/src/MusicButton.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import useSound from 'use-sound';
+
+import adventureStartSong from './assets/music/adventureStarts.ogg';
+import StopIcon from './icons/StopIcon';
+import PlayIcon from './icons/PlayIcon';
+
+export const MusicButton = () => {
+  const shouldPlayMusic = localStorage.getItem("shouldPlayMusic") === "true";
+  const [playMusic, setPlayMusic] = useState(shouldPlayMusic);
+
+  console.log({playMusic})
+
+  const [play, { stop }] = useSound(adventureStartSong, {
+    interrupt: true,
+    volume: 0.5,
+    loop: true,
+    autoplay: shouldPlayMusic,
+    onplay: () => setPlayMusic(true),
+    onstop: () => setPlayMusic(false),
+    onpause: () => setPlayMusic(false),
+  });
+
+  const handleMusic = () => {
+    if (playMusic) {
+      stop();
+      localStorage.setItem("shouldPlayMusic", "false");
+      setPlayMusic(false);
+    } else {
+      play();
+      localStorage.setItem("shouldPlayMusic", "true");
+      setPlayMusic(true);
+    }
+  };
+
+  return (
+    <button
+      className="bg-white color-black px-2 py-2 rounded-full"
+      onClick={handleMusic}
+    >
+      {playMusic ? (
+        <span className="w-4 h-4">
+          <StopIcon />
+        </span>
+      ) : (
+        <span className="w-4 h-4">
+          <PlayIcon />
+        </span>
+      )}
+    </button>
+  );
+};
