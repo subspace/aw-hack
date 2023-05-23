2db988fda51cada360a1b154d00182b42f314393
Natacha De la Rosa <delarosa.natacha@gmail.com>
2023-05-23 00:04:31 -0400

    feat(store): add store

--- packages/client/src/PlayerIcon.tsx
+++ packages/client/src/PlayerIcon.tsx
@@ -3,14 +3,16 @@ import player2Img from "./assets/images/player2.png";
 
 export const PlayerIcon = ({
   isMyPlayer = false,
+  additionalClass = "w-8 h-8",
 }: {
   isMyPlayer?: boolean;
+  additionalClass?: string;
 }) => {
   return (
     <div
-      className={`w-8 h-8 ${
+      className={`${
         isMyPlayer ? "bg-red-500" : "bg-blue-500"
-      } rounded-full flex items-center justify-center`}
+      } rounded-full flex items-center justify-center ${additionalClass}`}
     >
       <img
         src={isMyPlayer ? player1Img : player2Img}
